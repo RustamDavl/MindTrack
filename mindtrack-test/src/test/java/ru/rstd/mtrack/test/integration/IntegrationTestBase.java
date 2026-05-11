0a7bed4e80fb997b61aeb1e1a20e5ac237df2f5f
rustamdv <easton1234537@gmail.com>
2026-05-10 21:40:50 -0700

    MTR-4: feature add test configuration and first test

---
+++ mindtrack-test/src/test/java/ru/rstd/mtrack/test/integration/IntegrationTestBase.java
@@ -0,0 +1,27 @@
+package ru.rstd.mtrack.test.integration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import ru.rstd.mtrack.application.MindTrackAppRunner;
+
+@ActiveProfiles("test")
+@SpringBootTest(classes = MindTrackAppRunner.class)
+public abstract class IntegrationTestBase {
+
+    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:17");
+
+    @BeforeAll
+    static void init() {
+        postgreSQLContainer.start();
+    }
+
+    @DynamicPropertySource
+    static void postgreSQLContainerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+
+    }
+}
